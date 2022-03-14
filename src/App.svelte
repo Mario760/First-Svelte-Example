f42db5aadb4ef7f5de29b097cde5155f1abfb134
jiawe <jiaweima@cmail.carleton.ca>
2022-03-14 00:40:23 -0400

    Commit the svelte example

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	let name = 'Nested Components, HTML tags, and $:'
+	import Nested from "./Nested.svelte";
+	let urlString = "This is a boom example with a <strong>HTML</strong> " +
+			"and a hyperlink redirect to <a href='https://google.ca'>Google</a>";
+</script>
+
+<main>
+	<h1><b>Example for {name}!</b></h1>
+	<p>{@html urlString}</p>
+	<Nested count = {10}/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		background-color: -moz-mac-accentregularshadow;
+	}
+
+	h1 {
+		color: rgb(166,137,124);
+		font-family: "Times New Roman",cursive;
+		text-transform: uppercase;
+		font-size: 3em;
+		font-weight: 100;
+	}
+
+	p{
+		color: rgb(219,207,202);
+		font-size: 25px;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Nested.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let count;
+
+    let boomed = false;
+
+    let imageSrc = "https://c.tenor.com/4MOc_udzsRQAAAAd/explosion.gif";
+
+    $: if (count <= 0) {
+        alert('The Boom is Exploded!');
+        count = 0;
+        boomed = true;
+    }
+
+    function handleClick() {
+        count -= 1;
+    }
+
+</script>
+
+<button on:click={handleClick}>
+    Left {count} {count === 1 ? 'click' : 'clicks'}
+</button>
+
+{#if boomed}
+    <div>
+        <img src={imageSrc} alt="The boom exploded."/>
+    </div>
+{/if}
